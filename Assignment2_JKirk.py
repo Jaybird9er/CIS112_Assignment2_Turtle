c2f0b85d433c9bd7865b1c96f3e3fcc82fbe72dd
Jamey Kirk <jameykirk@gmail.com>
2021-02-28 23:15:25 +0900

    Basic Box

--- Assignment2_JKirk.py
+++ Assignment2_JKirk.py
@@ -1,4 +1,15 @@
 import turtle
 s = turtle.getscreen()
 t = turtle.getturtle()
-turtle.done
+
+t.penup()
+#t.setpos(x=60, y=-60)
+t.pendown()
+while True:
+    t.lt(90)
+    t.fd(120)
+    if abs(turtle.pos())<1:
+        break
+
+#window remains open
+turtle.exitonclick()
